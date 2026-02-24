9b39dbff62e7e594a3d299263c49302306dc2e14
sarathibalamurugan <sarathigreen4@gmail.com>
2026-02-24 20:12:58 +0530

    init: add doctypes

---
+++ quickfix/quickfix/doctype/device_type/device_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Parthsarathi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Device Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
